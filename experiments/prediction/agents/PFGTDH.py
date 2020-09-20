d2ab0cd3b11f94065e48f429c7c7253ba1ff9b8d
swordshieldmouse <alke8882000@gmail.com>
2020-09-20 13:42:11 -0400

    separated OLO

--- experiments/prediction/agents/PFGTDH.py
+++ experiments/prediction/agents/PFGTDH.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as LA
 
 class PFGTDH:
     """
@@ -12,13 +13,15 @@ class PFGTDH:
 
         self.lower_bound = -1e5
         self.upper_bound = 1e5
-        self.wealth = params['wealth'] # has to be more than 0 and of dimension 2d
-        self.hints = np.ones(features * 2)
-        self.beta = np.zeros(features * 2) # betting fraction
+        self.wealth = np.array(params['wealth'])
+        self.hints = np.ones(2)
+        self.beta = np.zeros(2) # betting fraction, one for each of primal and dual
         self.theta = np.zeros(features) # primal variable
         self.y = np.zeros(features) # dual variable
-        self.G = np.zeros(features * 2)
-        self.u = np.random.uniform(size = 2 * features) # initial direction
+        self.G = np.zeros((features, 2))
+        self.u = np.random.uniform(size = (features, 2)) # initial direction for each of primal and dual
+        self.u /= self.norm(self.u)
+        self.A = np.zeros(2)
 
         # average weights
         self.avg_theta = np.zeros(features)
@@ -30,22 +33,18 @@ class PFGTDH:
         ## for numerical stability
         self.eps = 1e-8
 
-        ## stuff for ONS
-        self.A = np.zeros(features * 2)
+    def norm(self, g):
+        return LA.norm(g, axis = 0, keepdims = True)
 
-    def grad_norm(self, g):
-        return np.abs(g) # 2-norm
-
-    def vec_norm(self, v):
-        return np.abs(v) # 2-norm
 
     def update(self, x, a, r, xp, rho):
-
+        # do updates separately on theta and y
         # assume g = (g_\theta, -g_y)
         # choose point via coin-betting
-        u = np.concatenate((self.theta, self.y))
         v = self.beta * self.wealth
-        w = v * u
+        # print(v.shape, self.beta.shape, self.wealth.shape)
+        w = self.u * v.reshape((1, 2))
+        assert w.shape == (self.features, 2), w.shape
 
         # project if there are constraints
         z = np.clip(w, self.lower_bound, self.upper_bound)
@@ -53,30 +52,37 @@ class PFGTDH:
         # otherwise, don't project
         # z = w
 
-        theta = z[ : self.features]
-        y = z[self.features : ]
+        theta = z[:, 0]
+        y = z[:, 1]
+        assert np.isnan(theta).sum() == 0, "nans in theta"
+        assert np.isnan(y).sum() == 0, "nans in y"
 
         # construct the gradient
         M = np.outer(x, x)
         b = rho * r * x
         A = rho * np.outer(x, x - self.gamma * xp)
-        g = np.concatenate((-A.T @ y, A @ theta + M @ y - b))
+        g_theta = -A.T @ y
+        g_y = A @ theta + M @ y - b
+        g = np.stack((g_theta, g_y)).transpose((1, 0))
+        assert g.shape == (self.features, 2)
 
         # constrained reduction to unconstrained
-        S_Z_grad = np.zeros(2 * self.features)
-        for i in range(2 * self.features):
-            if w[i] < self.lower_bound:
-                S_Z_grad[i] = w[i] - self.lower_bound
-            elif w[i] > self.upper_bound:
-                S_Z_grad[i] = w[i] - self.upper_bound
-        gtilde = 1 / 2 * g + self.grad_norm(g) * S_Z_grad
-        ineq_ind = (self.grad_norm(gtilde) > self.hints).astype(int)
-        gtrunc = ineq_ind * (self.hints * gtilde / (self.grad_norm(gtilde) + self.eps)) + (1 - ineq_ind) * gtilde # added a numerical constant for stability
-        assert np.isnan(gtrunc).sum() == 0, "nans in gtrunc"
-        self.hints = np.maximum(self.hints, self.grad_norm(gtilde))
+        S_Z_grad = np.zeros((self.features, 2))
+        for i in range(self.features):
+            for j in range(2):
+                if w[i, j] < self.lower_bound:
+                    S_Z_grad[i, j] = w[i, j] - self.lower_bound
+                elif w[i, j] > self.upper_bound:
+                    S_Z_grad[i, j] = w[i, j] - self.upper_bound
+        gtilde = 1 / 2 * g + self.norm(g) * S_Z_grad
+        ineq_ind = (self.norm(gtilde) > self.hints).astype(int)
+        gtrunc = ineq_ind * (self.hints * gtilde / (self.norm(gtilde) + self.eps)) + (1 - ineq_ind) * gtilde # added a numerical constant for stability
+        assert np.isnan(gtrunc).sum() == 0, f"nans in gtrunc, {gtrunc}"
+        self.hints = np.maximum(self.hints, self.norm(gtilde).squeeze())
         
         # update betting parameters
-        s = gtrunc @ u
+        # print(gtrunc.shape, self.u.shape)
+        s = np.sum(gtrunc * self.u, axis = 0)
         m = s / (1 - self.beta * s)
         self.A += np.power(m, 2)
         beta_minarg1 = self.beta - 2 * m / ((2 - np.log(3)) * self.A + self.eps)
@@ -86,19 +92,17 @@ class PFGTDH:
         self.beta = np.maximum(beta_maxarg1, beta_maxarg2)
         assert np.isnan(self.beta).sum() == 0, "nans in beta"
         self.wealth -= s * v
+        # print(self.beta.shape)
 
         # update the weights
-        self.G += self.grad_norm(gtrunc) ** 2
+        self.G += self.norm(gtrunc) ** 2
         next_u = self.u - np.sqrt(2) * gtrunc / (2 * np.sqrt(self.G) + self.eps)
-        self.u = next_u / self.vec_norm(next_u)
-        self.theta, self.y = (next_u[ : self.features], next_u[self.features : ])
-        assert np.isnan(self.theta).sum() == 0, "nans in theta"
-        assert np.isnan(self.y).sum() == 0, "nans in y"
+        self.u = next_u / LA.norm(next_u)
 
         # update averages
         self.t += 1
-        self.avg_theta = self.avg_theta * (self.t - 1) / self.t + self.theta / self.t
-        self.avg_y = self.avg_y * (self.t - 1) / self.t + self.y / self.t
+        self.avg_theta = self.avg_theta * (self.t - 1) / self.t + theta / self.t
+        self.avg_y = self.avg_y * (self.t - 1) / self.t + y / self.t
 
         
 
