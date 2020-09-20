2ce958383dc88a6fae12de4cef194b1617697ab9
__ApplePi <a.john.jacobsen@gmail.com>
2020-09-20 14:57:38 -0600

    refactor GTD2 into batch/regular. BatchGTD2 now is the one with online-to-batch conversion

--- experiments/prediction/agents/GTD2.py
+++ experiments/prediction/agents/GTD2.py
@@ -12,7 +12,35 @@ class GTD2:
         self.w = np.zeros(features)
         self.h = np.zeros(features)
 
-        self.avw = np.zeros(features)
+    def update(self, x, a, r, xp, rho):
+        v = self.w.dot(x)
+        vp = self.w.dot(xp)
+
+        delta = r + self.gamma * vp - v
+        delta_hat = self.h.dot(x)
+
+        dw = rho * (delta_hat * x - self.gamma * delta_hat * xp)
+        dh = (rho * delta - delta_hat) * x
+
+        self.w = self.w + self.alpha * dw
+        self.h = self.h + self.eta * self.alpha * dh
+
+    def getWeights(self):
+        return self.w
+
+class BatchGTD2:
+    def __init__(self, features, params):
+        self.features = features
+        self.params = params
+
+        self.gamma = params['gamma']
+        self.alpha = params['alpha']
+        self.eta = params.get('eta', 1)
+
+        self.w = np.zeros(features)
+        self.h = np.zeros(features)
+
+        self.av_w = np.zeros(features)
         self.t=0.0
 
     def update(self, x, a, r, xp, rho):
@@ -30,7 +58,7 @@ class GTD2:
         self.w = self.w + self.alpha * dw
         self.h = self.h + self.eta * self.alpha * dh
 
-        self.avw += 1.0/self.t * (self.w - self.avw)
+        self.av_w += 1.0/self.t * (self.w - self.avw)
 
     def getWeights(self):
-        return self.avw
+        return self.av_w
