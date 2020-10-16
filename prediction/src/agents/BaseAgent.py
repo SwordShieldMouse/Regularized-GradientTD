grads/_apply bugfix for non-gradient methods

prediction/src/agents/BaseAgent.py
@@ -6,10 +6,13 @@ class BaseAgent:
         self.actions = actions
         self.params = params
 
+        # used in batch_update
+        self.paramShape = (2, self.features)
+
     def batch_update(self, gen):
         num = self.params['batch_size']
         exps = gen.sample(samples=num)
-        shape = (num,) + (2, self.features)
+        shape = (num,) + self.paramShape
         grads = np.zeros(shape)
         for i in range(num):
             grads[i] = self.grads(*exps[i])

prediction/src/agents/TD.py
@@ -9,18 +9,24 @@ class TD(BaseAgent):
         self.alpha = params['alpha']
         self.w = np.zeros(features)
 
+        # for batch_update() to work efficiently, want to
+        # stack the param vectors and thus need their dimension.
+        # TD only needs 1 param vector, whereas generally the
+        # gradient methods need 2
+        self.paramShape = (1, self.features)
+
     def grads(self, x, a, xp, r, gamma, rho):
         v = self.w.dot(x)
         vp = self.w.dot(xp)
 
         delta = r + gamma * vp - v
-        return rho * delta * x, None
+        return rho * delta * x
 
     def update(self, x, a, xp, r, gamma, rho):
-        dw, dh = self.grads(x, a, xp, r, gamma, rho)
-        self._apply(dw, dh)
+        dw = self.grads(x, a, xp, r, gamma, rho)
+        self._apply(dw)
 
-    def _apply(self, dw, dh):
+    def _apply(self, dw):
         self.w = self.w + self.alpha * dw
 
     def initWeights(self, u):

prediction/src/agents/Vtrace.py
@@ -1,13 +1,10 @@
 import numpy as np
 
-from src.agents.BaseAgent import BaseAgent
+from src.agents.TD import TD
 
-class Vtrace(BaseAgent):
+class Vtrace(TD):
     def __init__(self, features, actions, params):
         super().__init__(features, actions, params)
-
-        self.alpha = params['alpha']
-        self.w = np.zeros(features)
 
     def grads(self, x, a, xp, r, gamma, rho):
         v = self.w.dot(x)
@@ -16,13 +13,13 @@ class Vtrace(BaseAgent):
         delta = r + gamma * vp - v
 
         rho_hat = np.min((rho, 1))
-        return rho_hat * delta * x, None
+        return rho_hat * delta * x
 
     def update(self, x, a, xp, r, gamma, rho):
-        dw, dh = self.grads(x, a, xp, r, gamma, rho)
-        self._apply(dw, dh)
+        dw = self.grads(x, a, xp, r, gamma, rho)
+        self._apply(dw)
 
-    def _apply(self, dw, dh):
+    def _apply(self, dw):
         self.w = self.w + self.alpha * dw
 
     def initWeights(self, u):