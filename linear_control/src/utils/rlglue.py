77eb2f1605fcdedcea5c51d8de80c38a2fae988b
__ApplePi <a.john.jacobsen@gmail.com>
2020-10-20 15:55:49 -0600

    rlglue hacks and bugfix

--- linear_control/src/utils/rlglue.py
+++ linear_control/src/utils/rlglue.py
@@ -23,13 +23,11 @@ class OneStepWrapper(BaseAgent):
 
         self.agent.update(self.x, self.a, xp, r, self.gamma)
 
-        ap = self.agent.selectAction(self.x)
-
         self.s = sp
-        self.a = ap
+        self.a = self.agent.selectAction(xp)
         self.x = xp
 
-        return ap
+        return self.a
 
     def end(self, r):
         gamma = 0
@@ -38,15 +36,53 @@ class OneStepWrapper(BaseAgent):
 
         # reset agent here if necessary (e.g. to clear traces)
 
-class PolicyWrapper(BaseAgent):
-    def __init__(self, policy):
-        self.policy = policy
-
-    def step(self, r, sp):
-        return self.policy.selectAction(sp)
+class OfflineOneStepWrapper(OneStepWrapper):
+    def __init__(self, agent, behavior, gamma, rep):
+        super().__init__(agent, gamma, rep)
+        self.behavior = behavior
 
     def start(self, s):
-        return self.policy.selectAction(s)
+        self.s = s
+        self.x = self.rep.encode(s)
+        self.a = self.behavior.selectAction(self.s)
+
+        return self.a
+
+    def step(self, r, sp):
+        xp = self.rep.encode(sp)
+
+        self.agent.update(self.x, self.a, xp, r, self.gamma)
+
+        self.s = sp
+        self.a = self.behavior.selectAction(sp)
+        self.x = xp
+
+        return self.a
+
+class PolicyWrapper(BaseAgent):
+    def __init__(self, agent, rep):
+        self.agent = agent
+        self.rep = rep
+
+        self.s = None
+        self.a = None
+        self.x = None
+
+    def start(self, s):
+        self.s = s
+        self.x = self.rep.encode(s)
+        self.a = self.agent.selectAction(self.x)
+
+        return self.a
+
+    def step(self, r, sp):
+        xp = self.rep.encode(sp)
+
+        self.s = sp
+        self.a = self.agent.selectAction(xp)
+        self.x = xp
+
+        return self.a
 
     def end(self, r):
-        return None
+        pass
