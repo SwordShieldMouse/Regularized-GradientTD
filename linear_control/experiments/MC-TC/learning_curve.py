dc5cf54bbc683b283986826f87067b37597d210b
__ApplePi <a.john.jacobsen@gmail.com>
2020-10-25 14:11:14 -0600

    experiments

--- linear_control/experiments/MC-TC/learning_curve.py
+++ linear_control/experiments/MC-TC/learning_curve.py
@@ -1,15 +1,49 @@
 import os
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 sys.path.append(os.getcwd())
 
-import matplotlib.pyplot as plt
-from analysis.learning_curve import generatePlot
+from src.analysis.learning_curve import plotBest
+from src.analysis.colors import colors
+from src.experiment import ExperimentModel
+from PyExpUtils.results.results import loadResults
+from PyExpUtils.utils.arrays import first
 
 TITLE="MountainCar"
 SAVE_PATH=os.path.join(os.getcwd(),"figures/MountainCar")
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-EXP_PATHS = list(map(lambda alg: os.path.join(FILE_DIR, f'{alg}.json'), ['PFGQ','PFGQ2', 'GQ', 'QC','QRC', 'QLearning']))
+EXP_PATHS = list(map(lambda alg: os.path.join(FILE_DIR, f'{alg}.json'),['PFGQ','PFRobust', 'QC','QRC', 'QLearning']))
+
+
+def getBest(results):
+    best = first(results)
+    bestVal = np.mean(best.load()[0])
+
+    for r in results:
+        a = r.load()[0]
+        am = np.mean(a[-1000:])
+        if am > bestVal:
+            best = r
+            bestVal = am
+
+    return best
+
+def generatePlot(ax, exp_paths, bounds):
+    for exp_path in exp_paths:
+        exp = ExperimentModel.load(exp_path)
+
+        results = loadResults(exp, 'return_summary.npy')
+
+        best = getBest(results)
+        print('best parameters:', exp_path)
+        print(best.params)
+
+        alg = exp.agent
+
+        b = plotBest(best, ax, label=alg, color=colors[alg])
+        bounds.append(b)
 
 if __name__ == "__main__":
     f, axes = plt.subplots(1)
