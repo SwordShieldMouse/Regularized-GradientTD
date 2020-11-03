3ea3eb301084af217cc4a688475ef2cc17e49c51
__ApplePi <a.john.jacobsen@gmail.com>
2020-11-03 14:59:59 -0700

    Revert "selectiev uniform avg"
    This reverts commit ee667ddc9c96dbca3ca3b5a9bc5210712907fc87.

--- prediction/src/utils/Averages.py
+++ prediction/src/utils/Averages.py
@@ -27,32 +27,6 @@ class WeightedAverage:
     def _get(self):
         raise(NotImplementedError('WeightedAverage: get not implemented'))
 
-class SelectiveUniform(WeightedAverage):
-    def __init__(self, x0):
-        self.t = np.zeros_like(x0) + 1.0
-        self.x = np.zeros_like(x0)
-        self.update(x0)
-
-    def update(self, x):
-        self._update(x)
-        wt = self._get()
-        self.x += np.multiply(wt , (x -self.x))
-
-    def reset(self, x):
-        self.x = np.zeros_like(x)
-        self.t = np.zeros_like(x)
-        self.update(x)
-
-    def get(self):
-        return self.x.copy()
-
-    def _update(self, x):
-        changed = np.argwhere(self.x != x)
-        self.t[changed] += 1.0
-
-    def _get(self):
-        return 1.0 / self.t
-
 class LastIterate(WeightedAverage):
     def __init__(self, x0):
         super().__init__(x0)
