fd76a9cf442d20ee276859c91280df4ca3cc9cc9
Andrew <a.john.jacobsen@gmail.com>
2021-02-02 18:25:26 -0700

    rm control

--- linear_control/src/agents/QC.py
+++
@@ -1,6 +0,0 @@
-from PyExpUtils.utils.dict import merge
-from src.agents.QRC import QRC
-
-class QC(QRC):
-    def __init__(self, features, actions, params):
-        super().__init__(features, actions, merge(params, { 'beta': 0 }))
